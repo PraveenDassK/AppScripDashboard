17957aa8a5f307f2217c82d7f332420829a112b8
PraveenDassK <praveendass@yahoo.com>
2023-10-05 17:21:12 +0530

    upcoming tab container created

--- taskdashboard/src/pages/Clinical/index.js
+++ taskdashboard/src/pages/Clinical/index.js
@@ -1,14 +1,36 @@
-import React from "react";
-import Header from "../../components/Header/Header";
-import SideBar from "../../components/SideBar/SideBar";
+import React, { useState } from "react";
 import Layout from "../../components/Layout/Layout";
+import "./index.css";
 const Clinical = () => {
+  const [selected, setSelected] = useState(0);
   return (
     <Layout>
-      <div style={{ backgroundColor: "#f4f4f4",width:"100%",height:"100vh" }}>
-        {/* <Header />
-      <SideBar /> */}
-        <div>Clinical</div>
+      <div
+        style={{ backgroundColor: "#f4f4f4", width: "100%", height: "100vh" }}>
+        <div className='clinic_tab-conatiner'>
+          <div className='clinic_tab-title'>Tasks</div>
+          <div className='clinic-tab-subTitle'>{`Clinical > Tasks`}</div>
+          <div className='clinic_tab-selector'>
+            <div
+              className={`${
+                selected === 0
+                  ? "clinic_selected-tab"
+                  : "clinic_not-selected-tab"
+              }`}
+              onClick={() => setSelected(0)}>
+              Upcoming Tasks
+            </div>
+            <div
+              className={`${
+                selected === 1
+                  ? "clinic_selected-tab"
+                  : "clinic_not-selected-tab"
+              }`}
+              onClick={() => setSelected(1)}>
+              Completed Tasks
+            </div>
+          </div>
+        </div>
       </div>
     </Layout>
   );
